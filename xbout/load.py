b0e4d283d35d586db203d8709644125ade9fd244
John Omotani <john.omotani@ukaea.uk>
2019-07-24 23:27:16 +0100

    Comment to remind me to fix loading geometry without x/y boundaries

--- xbout/load.py
+++ xbout/load.py
@@ -94,6 +94,7 @@ def open_boutdataset(datapath='./BOUT.dmp.*.nc',
     ds = _set_attrs_on_all_vars(ds, 'options', options)
 
     if gridfilepath:
+        # probably need to pass keep_xboundaries and keep_yboundaries here
         ds = open_grid(gridfilepath=gridfilepath, geometry=geometry,
                        coordinates=None, ds=ds,
                        keep_xboundaries=keep_xboundaries,
